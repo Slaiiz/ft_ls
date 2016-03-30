aa8a11899a8b51f7c02f672116515693d010bb5f
Slaiiz <crocoshack@gmail.com>
2016-03-30 16:25:29 +0200

    Added file include/ft_ls.h

---
+++ include/ft_ls.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/30 14:23:15 by vchesnea          #+#    #+#             */
+/*   Updated: 2016/03/30 14:23:16 by vchesnea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+# include <time.h>
+# include <errno.h>
+# include <stdio.h>
+# include <string.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/xattr.h>
+
+# include "libft.h"
+
+# define F_LIST			1
+# define F_RECURSIVE	2
+# define F_ALL			4
+# define F_REVERSE		8
+# define F_TIME		   16
+
+typedef struct	s_state
+{
+	const char	*exec;
+	short		flags;
+}				t_state;
+
+int				explore_path(const char *path, t_state *state);
+
+#endif
