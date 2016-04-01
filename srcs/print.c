0a292b4e91ab49631fdb7ce70232de0f5b1a79e1
Slaiiz <crocoshack@gmail.com>
2016-04-01 19:11:52 +0200

    Added 42 header. Tweaked functions to play okay with the recent changes.

--- srcs/print.c
+++ srcs/print.c
@@ -1,17 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/01 18:52:25 by vchesnea          #+#    #+#             */
+/*   Updated: 2016/04/01 18:52:27 by vchesnea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ls.h"
 
-void	print_filename(t_filedesc *desc, t_state *state, int padding)
+void	print_filename(t_file *file, t_query *query, int pad)
 {
-	while (padding--)
+	while (pad--)
 		ft_putchar(' ');
-	if (state->flags & F_COLOR)
+	if (query->flags & F_COLOR)
 	{
-		if (desc->stats.st_mode & S_IXUSR)
+		if (file->stats.st_mode & S_IXUSR)
 			ft_printf("{{red}}");
-		else if (desc->stats.st_mode & S_IFDIR)
+		else if (file->stats.st_mode & S_IFDIR)
 			ft_printf("{{blue}}");
 	}
-	ft_printf("%s", desc->filename);
-	if (state->flags & F_COLOR)
+	ft_printf("%s", file->ent->d_name);
+	if (query->flags & F_COLOR)
 		ft_printf("{{eoc}}");
+}
+
+void	print_access(t_file *file, t_query *query)
+{
+	ft_putchar(file->stats.st_mode & S_IFDIR ? 'd' : '-');
+	ft_putchar(file->stats.st_mode & S_IRUSR ? 'r' : '-');
+	ft_putchar(file->stats.st_mode & S_IWUSR ? 'w' : '-');
+	ft_putchar(file->stats.st_mode & S_IXUSR ? 'x' : '-');
+	ft_putstr(" ");
 }
