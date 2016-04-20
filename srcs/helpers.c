5c8194ff8e3533728c57c3ced2bb6e5d6fe5d8f9
Slaiiz <crocoshack@gmail.com>
2016-04-20 16:12:48 +0200

    Removed a useless function, moved a function from listing.c to here.

--- srcs/helpers.c
+++ srcs/helpers.c
@@ -6,11 +6,54 @@
 /*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 18:52:06 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/04/10 16:31:54 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/04/20 16:01:26 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+
+void		sort_directories(t_dir *dir)
+{
+	
+}
+
+void		sort_files(t_file *file)
+{
+
+}
+
+/*
+** set_query_paddings: Take note of the longest strings ft_ls is going to have
+** to print, so as to keep every information perfectly aligned.
+*/
+
+void		set_query_paddings(t_query *query)
+{
+	t_dir	*dir;
+	t_file	*file;
+
+	dir = query->listing;
+	while (dir)
+	{
+		file = dir->files;
+		while (file)
+		{
+			if (ft_nbrlen(file->stats.st_nlink, 10) > query->link_pad)
+				query->link_pad = ft_nbrlen(file->stats.st_nlink, 10);
+			if (ft_strlen(file->pwuid->pw_name) > query->user_pad)
+				query->user_pad = ft_strlen(file->pwuid->pw_name);
+			if (ft_strlen(file->grgid->gr_name) > query->grup_pad)
+				query->grup_pad = ft_strlen(file->grgid->gr_name);
+			if (ft_nbrlen(file->stats.st_size, 10) > query->size_pad)
+				query->size_pad = ft_nbrlen(file->stats.st_size, 10);
+			if (ft_strlen(file->name) > query->name_pad)
+				query->name_pad = ft_strlen(file->name);
+			file = file->next;
+		}
+		dir = dir->next;
+	}
+}
 
 /*
 ** strip_slashes: Return the text preceeding the rightmost slashes '/'.
@@ -65,14 +108,4 @@ int			is_listing_ordered(t_dir *listing)
 		curr = curr->next;
 	}
 	return (1);
-}
-
-/*
-** is_a_directory: Return TRUE if the given path implies a directory.
-** Else, return FALSE.
-*/
-
-int			is_a_directory(const char *path)
-{
-	return (path[ft_strlen(path) - 1] == '/');
 }
