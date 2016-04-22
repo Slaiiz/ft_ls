8acacd5961eba348dee4715514be569917e06a21
Slaiiz <crocoshack@gmail.com>
2016-04-22 16:14:48 +0200

    Removed two now useless functions.

--- srcs/helpers.c
+++ srcs/helpers.c
@@ -12,46 +12,14 @@
 
 #include "ft_ls.h"
 
-// void		sort_directories(t_dir *dir)
-// {
-//
-// }
-//
-// void		sort_files(t_file *file)
-// {
-//
-// }
-
-/*
-** set_query_paddings: Take note of the longest strings ft_ls is going to have
-** to print, so as to keep every information perfectly aligned.
-*/
-
-void		set_query_paddings(t_query *query)
+void		attach_data(t_file *file, struct stat *stats, char *path)
 {
-	t_dir	*dir;
-	t_file	*file;
-
-	dir = query->listing;
-	while (dir != NULL)
-	{
-		file = dir->files;
-		while (file != NULL)
-		{
-			if (ft_nbrlen(file->stats.st_nlink, 10) > query->link_pad)
-				query->link_pad = ft_nbrlen(file->stats.st_nlink, 10);
-			if (ft_strlen(file->pwuid->pw_name) > query->user_pad)
-				query->user_pad = ft_strlen(file->pwuid->pw_name);
-			if (ft_strlen(file->grgid->gr_name) > query->grup_pad)
-				query->grup_pad = ft_strlen(file->grgid->gr_name);
-			if (ft_nbrlen(file->stats.st_size, 10) > query->size_pad)
-				query->size_pad = ft_nbrlen(file->stats.st_size, 10);
-			if (ft_strlen(file->name) > query->name_pad)
-				query->name_pad = ft_strlen(file->name);
-			file = file->next;
-		}
-		dir = dir->next;
-	}
+	file->path = path;
+	file->stats = *stats;
+	file->pwuid = malloc(sizeof(struct passwd));
+	ft_memcpy(file->pwuid, getpwuid(stats->st_uid), sizeof(struct passwd));
+	file->grgid = malloc(sizeof(struct group));
+	ft_memcpy(file->grgid, getgrgid(stats->st_gid), sizeof(struct group));
 }
 
 /*
@@ -93,23 +61,34 @@ int			get_directory_blocksize(t_dir *dir)
 }
 
 /*
-** is_listing_ordered: Return TRUE if the given listing is correctly ordered.
-** Else, return FALSE.
+** set_query_paddings: Take note of the longest strings ft_ls is going to have
+** to print, so as to keep every information perfectly aligned.
 */
 
-int			is_listing_ordered(t_dir *listing)
+int			set_query_paddings(t_query *query)
 {
-	t_dir	*prev;
-	t_dir	*curr;
+	t_dir	*dir;
+	t_file	*file;
 
-	prev = listing;
-	curr = prev->next;
-	while (curr != NULL)
+	dir = query->listing;
+	while (dir != NULL)
 	{
-		if (ft_strcmp(prev->name, curr->name) > 0)
-			return (0);
-		prev = curr;
-		curr = curr->next;
+		file = dir->files;
+		while (file != NULL)
+		{
+			if (ft_nbrlen(file->stats.st_nlink, 10) > dir->link_pad)
+				dir->link_pad = ft_nbrlen(file->stats.st_nlink, 10);
+			if (ft_strlen(file->pwuid->pw_name) > dir->user_pad)
+				dir->user_pad = ft_strlen(file->pwuid->pw_name);
+			if (ft_strlen(file->grgid->gr_name) > dir->grup_pad)
+				dir->grup_pad = ft_strlen(file->grgid->gr_name);
+			if (ft_nbrlen(file->stats.st_size, 10) > dir->size_pad)
+				dir->size_pad = ft_nbrlen(file->stats.st_size, 10);
+			if (ft_strlen(file->name) > dir->name_pad)
+				dir->name_pad = ft_strlen(file->name);
+			file = file->next;
+		}
+		dir = dir->next;
 	}
-	return (1);
+	return (0);
 }
