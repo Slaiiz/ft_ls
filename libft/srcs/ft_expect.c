54a9651446a33d3547096d1b54d714fe574eb440
Slaiiz <crocoshack@gmail.com>
2016-06-01 17:00:15 +0200

    Updated that.

--- libft/srcs/ft_expect.c
+++ libft/srcs/ft_expect.c
@@ -6,7 +6,7 @@
 /*   By: vchesnea <vchesnea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/00/21 15:32:34 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/00/21 15:32:34 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/06/01 14:00:09 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,56 +17,51 @@ static void	read_number(const char **in, va_list argp)
 {
 	const char	*tmp;
 
-	ft_printf("%s -> ", *in);
 	tmp = *in;
 	while (ft_isdigit(*tmp))
-		tmp++;
+		++tmp;
 	if (!(ptrdiff_t)(tmp - *in))
 		return ;
 	*va_arg(argp, size_t*) = ft_atoi(*in);
-	ft_printf("%d\n", ft_atoi(*in));
 	*in = tmp;
 	return ;
 }
 
 static void	read_identifier(const char **in, va_list argp)
 {
-	const char	*tmp;
 	ptrdiff_t	len;
+	const char	*tmp;
 
-	ft_printf("%s -> ", *in);
 	tmp = *in;
 	while (ft_isalpha(*tmp))
 		tmp++;
 	if (!(len = (ptrdiff_t)(tmp - *in)))
 		return ;
 	*va_arg(argp, char**) = ft_strsub(*in, 0, len);
-	ft_printf("%s\n", ft_strsub(*in, 0, len));
 	*in = tmp;
 	return ;
 }
 
 int			ft_expect(const char **s, const char *format, ...)
 {
-	va_list	argp;
-	char	c;
+	char		c;
+	va_list		argp;
+	const char	*tmp;
 
+	tmp = *s;
 	va_start(argp, format);
-	while ((c = *format) != '\0')
+	while ((c = *format++) != '\0')
 	{
-		if (c == '$' && (c = *++format))
+		if (c == '$' && (c = *format++))
 		{
 			if (c == 'n')
-				read_number(s, argp);
+				read_number(&tmp, argp);
 			else if (c == 'i')
-				read_identifier(s, argp);
-			format++;
-			continue ;
+				read_identifier(&tmp, argp);
 		}
-		if (**s != c)
+		else if (*tmp++ != c)
 			return (0);
-		format++;
-		(*s)++;
 	}
+	*s = tmp;
 	return (1);
 }
